10b7dfde83b98da6ed48941ad351ad40b8941611
ZhuKsSer <43522049+ZhuKsSer@users.noreply.github.com>
2018-11-06 10:43:58 +0300

    Create lesson-06-hm-easy.py

---
+++ lesson-06-hm-easy.py
@@ -0,0 +1,83 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
+# Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
+import math
+
+class Triangle():
+    def __init__(self, a_x, a_y, b_x, b_y, c_x, c_y):
+        self.a_x = a_x
+        self.a_y = a_y
+        self.b_x = b_x
+        self.b_y = b_y
+        self.c_x = c_x
+        self.c_y = c_y
+        self.AB = round (math.sqrt(int (math.fabs(((b_y - a_y)**2) + ((b_x - a_x)**2)))),2)
+        self.BC = round(math.sqrt(int(math.fabs(((c_y - b_y) ** 2) + ((c_x - b_x) ** 2)))), 2)
+        self.CA = round(math.sqrt(int(math.fabs(((a_y - c_y) ** 2) + ((a_x - c_x) ** 2)))), 2)
+
+    def perimetr(self):
+        self.perimetr = (self.AB + self.BC + self.CA)
+        return(self.perimetr)
+
+    def ploshad(self):
+        self.perimetr /=2
+        self.ploshad =  round(math.sqrt(self.perimetr*(self.perimetr - self.AB)*(self.perimetr - self.BC)* (self.perimetr - self.CA)),2)
+        return(self.ploshad)
+
+    def vysota(self):
+        self.ploshad *=2
+        self.vysota =  round((self.ploshad / self.CA),2)
+        return(self.vysota)
+
+treugolnik1 = Triangle(3, 2, 6, 7, 0, 12)
+
+print(treugolnik1.perimetr())
+print(treugolnik1.ploshad())
+print(treugolnik1.vysota())
+
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
+# Предусмотреть в классе методы:
+# проверка, является ли фигура равнобочной трапецией;
+# вычисления: длины сторон, периметр, площадь.
+import math
+
+class Trapeze:
+    def __init__(self, A, B, C, D):
+        # Функция вычисляет длину стороны согласно координатам точек
+        def sideLen(dot1, dot2):
+            return math.sqrt((dot1[0] - dot2[0]) ** 2
+                             + (dot1[1] - dot2[1}) ** 2)
+
+        def areaTriangle(len1, len2, len3):
+            semi_perimeter = (len1 + len2 + len3) / 2
+
+            return math.sqrt(semi_perimeter
+                             * (semi_perimeter - len1)
+                             * (semi_perimeter - len2)
+                             * (semi_perimeter - len3))
+
+        self.A = A
+        self.B = B
+        self.C = C
+        self.D = D
+
+        self.AB = sideLen(self.A, self.B)
+        self.BC = sideLen(self.B, self.C)
+        self.CD = sideLen(self.C, self.D)
+        self.DA = sideLen(self.D, self.A)
+        self.diagonal_AC = sideLen(self.C, self.A)
+        self.diagonal_BD = sideLen(self.B, self.D)
+        self.perimeter = self.AB + self.BC + self.CD + self.DA
+
+        # представим трапецию как 2 треугольника и сложим 2 площади этих треугольников
+        # Для этого у нас есть все необходимые длины сторон
+        self.area = areaTriangle(self.AB, self.diagonal_BD, self.DA) \
+                    + areaTriangle(self.diagonal_BD, self.BC, self.CD)
+
+    def isTrapezeEqu(self):
+        if self.diagonal_AC == self.diagonal_BD:
+            return True
+        return False
